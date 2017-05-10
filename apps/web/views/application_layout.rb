e5ef1ff6a7e1173804a0e3e22401308438849438
Anton Davydov <antondavydov.o@gmail.com>
2017-05-11 01:44:43 +0300

    Wrap footer to ruby helper

--- apps/web/views/application_layout.rb
+++ apps/web/views/application_layout.rb
@@ -2,6 +2,19 @@ module Web
   module Views
     class ApplicationLayout
       include Web::Layout
+
+      def footer
+        html.div(class: 'footer row') do
+          div(class: 'col-md-4 col-md-offset-4 center') do
+            text '© 2017'
+            a(href: '#') { 'Anton Davydov,' }
+            text 'et al —'
+            a(href: '#') { 'Source' }
+            text 'available under'
+            a(href: '#') { 'MIT' }
+          end
+        end
+      end
     end
   end
 end

27cc71b9c195ac3aac36aa716a74801df32dbfb9
Anton Davydov <antondavydov.o@gmail.com>
2017-05-15 00:00:06 +0300

    Add migration for projects table

---
+++ db/migrations/20170514004347_create_projects.rb
@@ -0,0 +1,21 @@
+Hanami::Model.migration do
+  change do
+    create_table :projects do
+      primary_key :id
+      foreign_key :account_id, :accounts, on_delete: :cascade, null: false
+
+      column :url,         String
+      column :name,        String, null: false
+      column :owner,       String
+      column :language,    String, default: 'undefined'
+      column :description, String
+      column :private,     FalseClass, default: false
+
+      column :stargazers_count, Integer
+
+      column :starred_at, DateTime, null: false
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
+  end
+end
--- lib/octostar/repositories/account_repository.rb
+++ lib/octostar/repositories/account_repository.rb
@@ -1,4 +1,8 @@
 class AccountRepository < Hanami::Repository
+  associations do
+    has_many :projects
+  end
+
   def find_by_login(login)
     accounts.where(login: login).one
   end

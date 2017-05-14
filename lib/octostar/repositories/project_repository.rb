ae1b147f2d1258ead4d7a983bdbbcf50566d19f7
Anton Davydov <antondavydov.o@gmail.com>
2017-05-15 02:29:58 +0300

    Add search query for getting posts

--- lib/octostar/repositories/project_repository.rb
+++ lib/octostar/repositories/project_repository.rb
@@ -1,5 +1,13 @@
 class ProjectRepository < Hanami::Repository
-  def find_by_account(account_id)
-    projects.where(account_id: account_id).as(Project).to_a
+  def find_by_account(account_id, text = nil)
+
+    query = projects.where(account_id: account_id)
+
+    if text
+      text = "%#{text}%"
+      query = query.where { name.like(text) | description.like(text) }
+    end
+
+    query.as(Project).to_a
   end
 end

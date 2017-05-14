e967818fc26884d36f74c971a693b87352a556df
Anton Davydov <antondavydov.o@gmail.com>
2017-05-15 01:08:32 +0300

    Display all projects on index page

--- apps/web/controllers/repos/index.rb
+++ apps/web/controllers/repos/index.rb
@@ -1,8 +1,16 @@
 module Web::Controllers::Repos
   class Index
     include Web::Action
+    expose :projects
 
     def call(params)
+      @projects = repo.find_by_account(current_account.id) if authenticated?
+    end
+
+    private
+
+    def repo
+      @repo ||= ProjectRepository.new
     end
   end
 end
--- lib/octostar/repositories/project_repository.rb
+++ lib/octostar/repositories/project_repository.rb
@@ -1,2 +1,5 @@
 class ProjectRepository < Hanami::Repository
+  def find_by_account(account_id)
+    projects.where(account_id: account_id).as(Project).to_a
+  end
 end

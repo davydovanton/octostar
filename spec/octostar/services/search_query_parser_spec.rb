cec0b8c5f910e7060f1900fd04b14b2cce9716f7
Anton Davydov <antondavydov.o@gmail.com>
2017-05-16 01:12:18 +0300

    Add new tests

--- spec/octostar/services/search_query_parser_spec.rb
+++ spec/octostar/services/search_query_parser_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe SearchQueryParser do
   it { expect(SearchQueryParser.new('').call).to eq(text: nil) }
   it { expect(SearchQueryParser.new('text').call).to eq(text: 'text') }
   it { expect(SearchQueryParser.new('author:davydovanton').call).to eq(author: 'davydovanton', text: nil) }
-  it { expect(SearchQueryParser.new('author:davydovanton text').call).to eq(author: 'davydovanton', text: 'text') }
+  it { expect(SearchQueryParser.new('author:davydovanton lang:ruby text').call).to eq(author: 'davydovanton', lang: 'ruby', text: 'text') }
+  it { expect(SearchQueryParser.new('tag:test tag:other text').call).to eq(tag: %w[test other], text: 'text') }
 end

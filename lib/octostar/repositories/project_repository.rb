79d6f7e1538be908a689ab8084a8d0c64adb419c
Anton Davydov <antondavydov.o@gmail.com>
2017-05-15 03:09:34 +0300

    Add searching by author

--- lib/octostar/repositories/project_repository.rb
+++ lib/octostar/repositories/project_repository.rb
@@ -2,12 +2,29 @@ class ProjectRepository < Hanami::Repository
   def find_by_account(account_id, search = {})
 
     query = projects.where(account_id: account_id)
-
-    if search[:text]
-      text = "%#{text}%"
-      query = query.where { name.like(text) | description.like(text) }
-    end
+    query = text_search(query, search[:text])
+    query = author_search(query, search[:author])
 
     query.as(Project).to_a
+  end
+
+private
+
+  def text_search(query, text)
+    if text
+      text = "%#{text}%"
+      query.where { name.like(text) | description.like(text) }
+    else
+      query
+    end
+  end
+
+  def author_search(query, author)
+    if author
+      author = "%#{author}%"
+      query.where { owner.like(author) }
+    else
+      query
+    end
   end
 end

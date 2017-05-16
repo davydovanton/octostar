39ca88b7ae7d1fa490c0b4c19b7f78242cdfddd3
Anton Davydov <antondavydov.o@gmail.com>
2017-05-17 02:21:45 +0300

    Use ILIKE instead LIKE for case-insensitive matching

--- lib/octostar/repositories/project_repository.rb
+++ lib/octostar/repositories/project_repository.rb
@@ -1,6 +1,5 @@
 class ProjectRepository < Hanami::Repository
   def find_by_account(account_id, search = {})
-
     query = projects.where(account_id: account_id)
     query = text_search(query, search[:text])
     query = author_search(query, search[:author])
@@ -12,33 +11,37 @@ class ProjectRepository < Hanami::Repository
 
 private
 
+  def pattern(text)
+    "%#{text.downcase}%"
+  end
+
   def text_search(query, text)
     return query if text.nil?
 
-    text = "%#{text}%"
-    query.where { name.like(text) | description.like(text) }
+    text = pattern(text)
+    query.where { name.ilike(text) | description.ilike(text) }
   end
 
   def author_search(query, author)
     return query if author.nil?
 
-    author = "%#{author}%"
-    query.where { owner.like(author) }
+    author = pattern(author)
+    query.where { owner.ilike(author) }
   end
 
   def language_search(query, lang)
     return query if lang.nil?
 
-    lang = "%#{lang}%"
-    query.where { lower(language).like(lang) }
+    lang = pattern(lang)
+    query.where { language.ilike(lang) }
   end
 
   def tags_search(query, tag)
     return query if tag.nil?
 
     Array(tag).each do |topic|
-      topic = "%#{topic}%"
-      query = query.where { array_to_string(topics, ',').like(topic) }
+      topic = pattern(topic)
+      query = query.where { array_to_string(topics, ',').ilike(topic) }
     end
     query
   end
